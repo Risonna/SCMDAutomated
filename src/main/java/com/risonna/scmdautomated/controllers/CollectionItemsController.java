159591b3770bdfc001160a5891798cd0cae4f80c
Neeko <Risonnan@gmail.com>
2024-08-27 14:14:25 +0700

    Fixed a few bugs, changed settings window visually. Changed alert when the application is opened without steamcmd path specified. Added a folder button to collection items if they are present on the file system before opening the collection items window.

--- src/main/java/com/risonna/scmdautomated/controllers/CollectionItemsController.java
+++ src/main/java/com/risonna/scmdautomated/controllers/CollectionItemsController.java
@@ -11,16 +11,23 @@ import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -85,58 +92,92 @@ public class CollectionItemsController implements DownloadStatusObserver {
     }
 
     private HBox createItemPane(JSONObject publishedFileDetail) {
-        String title = publishedFileDetail.optString("title", null);
-        long fileSize = publishedFileDetail.optLong("file_size", -1);
-        String imageUrl = publishedFileDetail.optString("preview_url", "https://albums193.zbporn.com/main/9998x9998/98000/98571/2335763.jpg");
-        long publishedFileId = publishedFileDetail.optLong("publishedfileid", -1);
-        long appId = publishedFileDetail.optLong("consumer_app_id", -1);
+        try {
+            String title = publishedFileDetail.optString("title", null);
+            long fileSize = publishedFileDetail.optLong("file_size", -1);
+            String imageUrl = publishedFileDetail.optString("preview_url", "https://albums193.zbporn.com/main/9998x9998/98000/98571/2335763.jpg");
+            long publishedFileId = publishedFileDetail.optLong("publishedfileid", -1);
+            long appId = publishedFileDetail.optLong("consumer_app_id", -1);
 
-        if (title == null || fileSize == -1 || publishedFileId == -1 || appId == -1) {
-            System.out.println("Missing mandatory field(s) in JSON: " + publishedFileDetail.toString());
-            return null;
+            if (title == null || fileSize == -1 || publishedFileId == -1 || appId == -1) {
+                System.out.println("Missing mandatory field(s) in JSON: " + publishedFileDetail.toString());
+                return null;
+            }
+
+            HBox itemPane = new HBox(10);
+            itemPane.getStyleClass().add("item-pane");
+            itemPane.setAlignment(Pos.CENTER_LEFT);
+
+            String filePath = SettingsController.getSteamcmdPath() + "\\steamapps\\workshop\\content\\" + appId + "\\" + publishedFileId;
+
+            ImageView imageView = new ImageView(ImageDownloader.downloadImage(imageUrl));
+            imageView.setFitHeight(100);
+            imageView.setFitWidth(100);
+            imageView.setPreserveRatio(true);
+
+            HBox detailsBox = new HBox(5);
+            detailsBox.setAlignment(Pos.CENTER_LEFT);
+            HBox.setHgrow(detailsBox, Priority.ALWAYS);
+
+            VBox labelsAndButton = new VBox(5);
+            Label titleLabel = new Label(title);
+            titleLabel.getStyleClass().add("item-title");
+            Label sizeLabel = new Label("Size: " + getHumanReadableFileSize(fileSize));
+            sizeLabel.getStyleClass().add("item-size");
+
+            Button actionButton = new Button();
+            actionButton.getStyleClass().add("action-button");
+            actionButton.setUserData(publishedFileId);
+
+            VBox buttonContainer = new VBox(5);
+            buttonContainer.getChildren().add(actionButton);
+            labelsAndButton.getChildren().addAll(titleLabel, sizeLabel, buttonContainer);
+
+            detailsBox.getChildren().add(labelsAndButton);
+            itemPane.getChildren().addAll(imageView, detailsBox);
+
+            RecentDownload existingDownload = recentDownloadsController.getDownloadByPublishedFileId(String.valueOf(publishedFileId));
+            if (SteamCMDInteractor.isFileDownloaded(filePath)) {
+                // Item is already downloaded, show "Installed" button
+                actionButton.setText("Installed");
+                actionButton.getStyleClass().add("installed-button");
+                actionButton.setDisable(false);
+                actionButton.setOnAction( e -> retryDownload(String.valueOf(publishedFileId), title, imageView.getImage(), getHumanReadableFileSize(fileSize), appId));
+
+                ImageView folderIcon = new ImageView(new Image(getClass().getResourceAsStream("/com/risonna/scmdautomated/images/folder.png")));
+                folderIcon.setFitHeight(35);
+                folderIcon.setFitWidth(35);
+                folderIcon.setOnMouseClicked(event -> openFolder(filePath));
+
+                // Now add folderIcon to itemPane
+                itemPane.getChildren().add(folderIcon);
+            } else if (existingDownload != null) {
+                // Update button state based on existing download status
+                updateButtonState(existingDownload.getDownloadStatus(), actionButton);
+            } else {
+                // Item is not downloaded, show "Download" button
+                actionButton.setText("Download");
+                actionButton.setOnAction(e -> downloadItem(String.valueOf(publishedFileId), title, imageView.getImage(), String.valueOf(fileSize), appId));
+            }
+
+            itemPanes.put(String.valueOf(publishedFileId), itemPane);
+
+            animateItemPane(itemPane);
+            return itemPane;
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        HBox itemPane = new HBox(10);
-        itemPane.getStyleClass().add("item-pane");
-
-        String filePath = SettingsController.getSteamcmdPath() + "\\steamapps\\workshop\\content\\" + appId + "\\" + publishedFileId;
-
-        ImageView imageView = new ImageView(ImageDownloader.downloadImage(imageUrl));
-        imageView.setFitHeight(100);
-        imageView.setFitWidth(100);
-        imageView.setPreserveRatio(true);
-
-        VBox detailsBox = new VBox(5);
-        Label titleLabel = new Label(title);
-        titleLabel.getStyleClass().add("item-title");
-        Label sizeLabel = new Label("Size: " + getHumanReadableFileSize(fileSize));
-        sizeLabel.getStyleClass().add("item-size");
-
-        detailsBox.getChildren().addAll(titleLabel, sizeLabel);
-
-        Button actionButton = new Button();
-        actionButton.getStyleClass().add("action-button");
-        actionButton.setUserData(publishedFileId);
-
-        RecentDownload existingDownload = recentDownloadsController.getDownloadByPublishedFileId(String.valueOf(publishedFileId));
-        if (SteamCMDInteractor.isFileDownloaded(filePath)) {
-            actionButton.setText("Installed");
-            actionButton.getStyleClass().add("installed-button");
-            actionButton.setDisable(true);
-        } else if (existingDownload != null) {
-            updateButtonState(existingDownload.getDownloadStatus(), actionButton);
-        } else {
-            actionButton.setText("Download");
-            actionButton.setOnAction(e -> downloadItem(String.valueOf(publishedFileId), title, imageView.getImage(), String.valueOf(fileSize), appId));
+        return null;
+    }
+    private void openFolder(String filePath) {
+        try {
+            Desktop.getDesktop().open(new File(filePath));
+        } catch (IOException e) {
+            // Handle error if opening the folder fails
+            System.err.println("Failed to open folder: " + e.getMessage());
         }
-
-        detailsBox.getChildren().add(actionButton);
-        itemPane.getChildren().addAll(imageView, detailsBox);
-        itemPanes.put(String.valueOf(publishedFileId), itemPane);
-
-        animateItemPane(itemPane);
-
-        return itemPane;
     }
 
     private void updateButtonState(String status, Button actionButton) {
@@ -167,18 +208,26 @@ public class CollectionItemsController implements DownloadStatusObserver {
             downloadItem(publishedFileId, download.getName(), download.getImage(), download.getFileSize(), Long.parseLong(download.getAppId()));
         }
     }
+    private void retryDownload(String publishedFileId, String name, Image image, String size, long appId) {
+        if (publishedFileId != null && name != null && image != null && size != null && appId > 0) {
+            downloadItem(publishedFileId, name, image, size, appId);
+        }
+    }
 
     private void downloadItem(String publishedFileId, String title, Image image, String size, long appId) {
         String filePath = SettingsController.getSteamcmdPath() + "\\steamapps\\workshop\\content\\" + appId + "\\" + publishedFileId;
 
         if (SteamCMDInteractor.isFileDownloaded(filePath)) {
+            // Item is already downloaded, show redownload confirmation
             Alert alert = createRedownloadConfirmationDialog();
             Optional<ButtonType> result = alert.showAndWait();
 
             if (result.isPresent() && result.get() == ButtonType.OK) {
+                // User wants to redownload
                 startDownload(publishedFileId, title, image, size, appId);
             }
         } else {
+            // Item is not downloaded, start download
             startDownload(publishedFileId, title, image, size, appId);
         }
     }
@@ -194,8 +243,10 @@ public class CollectionItemsController implements DownloadStatusObserver {
     public void updateItemStatus(String publishedFileId, String status) {
         HBox itemPane = itemPanes.get(publishedFileId);
         if (itemPane != null) {
-            VBox detailsBox = (VBox) itemPane.getChildren().get(1);
-            Button actionButton = (Button) detailsBox.getChildren().get(detailsBox.getChildren().size() - 1);
+            HBox detailsBox = (HBox) itemPane.getChildren().get(1);
+            VBox labelsAndButtons = (VBox) detailsBox.getChildren().get(0);
+            VBox buttonContainer = (VBox) labelsAndButtons.getChildren().get(labelsAndButtons.getChildren().size() - 1);
+            Button actionButton = (Button) buttonContainer.getChildren().get(buttonContainer.getChildren().size() - 1);
             updateButtonState(status, actionButton);
         }
     }
